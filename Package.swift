b59413117133faf143f84aa0ba40b22609a1df38
mtfum <fumiya.yamanaka@plaid.co.jp>
2026-04-23 13:43:48 +0900

    fix: LiteRTLM も 0.1.4 のクリーンな zip に更新（Frameworks/ 混入を解消）

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LiteRTLM",
-            url: "https://github.com/mtfum/LiteRTLMSwift/releases/download/0.1.0/LiteRTLM.xcframework.zip",
-            checksum: "11fa21ba9617f5ca1b6162501b2fa5d34e4d8ea8f15eba408ace10be9a64dd32"
+            url: "https://github.com/mtfum/LiteRTLMSwift/releases/download/0.1.4/LiteRTLM_0.1.4.xcframework.zip",
+            checksum: "adf7f6da02a1cf2e510726e3c933ddc7f77038631f93e6b6312839bb231d08a7"
         ),
         .binaryTarget(
             name: "GemmaModelConstraintProvider",
